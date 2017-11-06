aa14afba2e47526eca446ea6a45d9714dcc48af6
Web Pro <web.devpro125@gmail.com>
2017-11-06 23:43:14 +0700

    schedule issue

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,4 @@
+require File.expand_path('../config/environment', __dir__)
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
