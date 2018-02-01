a4ac54a1b3b53b27e68653998b04c79a98f59a22
Web Pro <web.devpro125@gmail.com>
2018-02-01 11:06:34 -0500

    update notify court mail template for case number and type of hearing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,16 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  ActionMailer::Base.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'localhost:3000',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
 end
