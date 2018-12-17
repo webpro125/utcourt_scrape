a7e932271c8a6b95546d744a3fd45566240154cb
Sokom <web.devpro125@gmail.com>
2018-12-17 16:32:20 -0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 admin = Admin.find_or_initialize_by(email: 'sokomheng89@gmail.com')
-admin.password = 'flygare'
+admin.password = 'test1234'
 admin.save!
 
 admin = Admin.find_or_initialize_by(email: 'edward@stonelawfirm.net')
