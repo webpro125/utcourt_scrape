21c260b88f9def492cf85b1d76ad75c8c77f59d7
Sokom <web.devpro125@gmail.com>
2019-01-04 06:46:26 -0700

    change scheduled courts controller

--- app/controllers/scheduled_courts_controller.rb
+++ app/controllers/scheduled_courts_controller.rb
@@ -3,7 +3,7 @@ class ScheduledCourtsController < ApplicationController
   layout 'front'
 
   def index
-    @q = CourtCalendar.where(atty_last_name: current_user.first_name.downcase, atty_first_name: current_user.last_name.downcase).ransack(params[:q])
+    @q = CourtCalendar.where(atty_last_name: current_user.last_name.downcase, atty_first_name: current_user.first_name.downcase).ransack(params[:q])
     @q.sorts = ['start_date asc', 'start_time asc'] if @q.sorts.empty?
     @court_calendars = @q.result(distinct: true).page(params[:page]).per(15)
   end
